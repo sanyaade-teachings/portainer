b5d7aa9e4b8ab8dc06dad9a07d2f9bd273dbacf1
Chaim Lev-Ari <chiptus@gmail.com>
2020-08-05 12:18:42 +0300

    feat(extensions): remove server rbac code

--- api/portainer.go
+++ api/portainer.go
@@ -1259,8 +1259,6 @@ const (
 	RegistryManagementExtension
 	// OAuthAuthenticationExtension represents the OAuth authentication extension (Deprecated)
 	OAuthAuthenticationExtension
-	// RBACExtension represents the RBAC extension
-	RBACExtension
 )
 
 const (
